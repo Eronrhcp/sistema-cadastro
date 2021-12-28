88842f1198d8818daae185dfd0c7db57f02b6226
Eron Oliveira <eron.fisica@gmail.com>
2021-12-28 13:57:28 -0300

    update form.js

--- cliente/src/componentes/Form.js
+++ cliente/src/componentes/Form.js
@@ -3,6 +3,18 @@ import React, { useState } from 'react'
 function Form() {
     let [values, setValues] = useState()
 
+    console.log(values)
+
+    let handleChangeValues = (value) => {
+        setValues(prevValue => ({
+            ...prevValue,
+            [value.target.name]: value.target.value,
+        }))
+    }
+
+    let handleClickButton = ()=> {
+        console.log(values)
+    }
     return (
         <div className="bg-white 
         h-64 w-96 mx-auto 
@@ -14,20 +26,27 @@ function Form() {
                 name="nome"
                 placeholder="Nome"
                 className=""
+                onChange={handleChangeValues}
             />
             <input
                 type="text"
                 name="preco"
                 placeholder="Preço"
                 className=""
+                onChange={handleChangeValues}
             />
             <input
                 type="text"
                 name="categoria"
                 placeholder="Categoria"
                 className=""
+                onChange={handleChangeValues}
             />
-            <button className=''> Cadastrar </button>
+            <button
+                onClick={() => handleClickButton()}
+                className='
+            border-2 border-gray-600 rounded-md bg-gray-300 hover:bg-white
+            '> Cadastrar </button>
         </div>
     )
 }
